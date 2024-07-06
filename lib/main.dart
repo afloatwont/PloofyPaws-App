8c8ac12b01c232172c88c4cc26733410e2a27967
4YU5H25 <ayush4yush@gmail.com>
2024-07-06 11:28:33 +0530

    calendar issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ploofypaws/chat/services/chat_database_service.dart';
 import 'package:ploofypaws/config/theme/theme.dart';
+import 'package:ploofypaws/controllers/calender_provider.dart';
 import 'package:ploofypaws/controllers/time_provider.dart';
 import 'package:ploofypaws/location/map_location.dart';
 import 'package:ploofypaws/pages/home/services/pet_walking/selected_plan_provider.dart';
+import 'package:ploofypaws/pages/pet_onboarding/pet_onboard.dart';
 import 'package:ploofypaws/pages/profile/pet_life_event/memories.dart';
 import 'package:ploofypaws/pages/profile/pet_life_event/pet_memories.dart';
 import 'package:ploofypaws/pages/root/init_app.dart';
@@ -41,6 +43,7 @@ Future<void> main() async {
       ChangeNotifierProvider(create: (_) => UserProvider()),
       ChangeNotifierProvider(create: (_) => TimeProvider()),
       ChangeNotifierProvider(create: (_) => AddressModel()),
+      ChangeNotifierProvider(create: (_) => CalendarProvider()),
     ],
     child: const MyApp(),
   ));
@@ -99,7 +102,8 @@ class _MyAppState extends State<MyApp> {
         textTheme:
             GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.black),
       ),
-      home: _authServices.user != null ? const Root() : const InitApp(),
+      home:
+          _authServices.user != null ? const PetOnboarding() : const InitApp(),
     );
   }
 }
