1d2c8e9ff4dda6d3b7af81cf8e7bd2a952cacf8c
4YU5H25 <ayush4yush@gmail.com>
2024-07-25 17:10:34 +0530

    tracker main screen ui

--- lib/pages/tracker/device_found.dart
+++ lib/pages/tracker/device_found.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cupertino_battery_indicator/cupertino_battery_indicator.dart';
+import 'package:cupertino_battery_indicator/cupertino_battery_indicator.dart'
+    as cbi;
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:ploofypaws/components/adaptive_page_scaffold.dart';
 import 'package:ploofypaws/components/input_label.dart';
 import 'package:ploofypaws/config/theme/theme.dart';
+import 'package:ploofypaws/pages/tracker/map/map_widget.dart';
 import 'package:ploofypaws/services/repositories/auth/firebase/fire_assets.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class DeviceFound extends StatefulWidget {
   final BarcodeCapture? barcodeCapture;
@@ -18,6 +23,19 @@ class DeviceFound extends StatefulWidget {
 }
 
 class _DeviceFoundState extends State<DeviceFound> {
+  Future<void> fetchData(String imei) async {
+    final response = await http.get(Uri.parse(
+        'https://vahantrack.com/api/api.php?api=user&ver=1.0&key=E43FEC932566D9E32F1CD2DC3F5CAE01&cmd=OBJECT_GET_LOCATIONS,$imei'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)['861261029438534'];
+      final latitude = double.parse(data['lat']);
+      final longitude = double.parse(data['lng']);
+      final battery = double.parse(data['params']['bats']);
+      final trackerDateTime = DateTime.parse(data['dt_tracker']);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final urlProvider = context.read<UrlProvider>();
@@ -38,141 +56,150 @@ class _DeviceFoundState extends State<DeviceFound> {
                   separatorBuilder: (context, index) =>
                       const SizedBox(height: 16),
                   itemBuilder: (context, index) {
-                    return Container(
-                      // padding: const EdgeInsets.all(16),
-                      height: MediaQuery.sizeOf(context).height * 0.28,
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                            colors: [Color(0xff1A1A1A), Color(0xffB8E8E8E)],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Stack(
-                        children: [
-                          if (urlProvider.urlMap[
-                                  'assets/images/content/device_bg.png'] !=
-                              null)
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const MyMapWidget(),
+                        ));
+                      },
+                      child: Container(
+                        // padding: const EdgeInsets.all(16),
+                        height: MediaQuery.sizeOf(context).height * 0.28,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                              colors: [Color(0xff1A1A1A), Color(0xffB8E8E8E)],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          children: [
+                            if (urlProvider.urlMap[
+                                    'assets/images/content/device_bg.png'] !=
+                                null)
+                              Positioned(
+                                bottom: 0,
+                                child: CachedNetworkImage(
+                                  imageUrl: urlProvider.urlMap[
+                                      'assets/images/content/device_bg.png']!,
+                                  color: Colors.grey,
+                                  placeholder: null,
+                                  errorWidget: null,
+                                ),
+                              ),
                             Positioned(
-                              bottom: 0,
-                              child: CachedNetworkImage(
-                                imageUrl: urlProvider.urlMap[
-                                    'assets/images/content/device_bg.png']!,
-                                color: Colors.grey,
-                                placeholder: null,
-                                errorWidget: null,
-                              ),
-                            ),
-                          Positioned(
-                            top: 0,
-                            left: 0,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      CircleAvatar(
-                                        backgroundColor: Colors.transparent,
-                                        backgroundImage:
-                                            CachedNetworkImageProvider(
-                                          urlProvider.urlMap[
-                                              'assets/images/content/dog.png']!,
+                              top: 0,
+                              left: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        CircleAvatar(
+                                          backgroundColor: Colors.transparent,
+                                          backgroundImage:
+                                              CachedNetworkImageProvider(
+                                            urlProvider.urlMap[
+                                                'assets/images/content/dog.png']!,
+                                          ),
                                         ),
-                                      ),
-                                      const SizedBox(width: 16),
-                                      _buildHeader("Arlo's Tracker"),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 8),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "Version: ",
-                                        style: TextStyle(
-                                            color: Colors.grey[50],
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text("2g",
+                                        const SizedBox(width: 16),
+                                        _buildHeader("Arlo's Tracker"),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Version: ",
                                           style: TextStyle(
-                                              color: Colors.grey[50])),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 8),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "IMEI: ",
-                                        style: TextStyle(
-                                            color: Colors.grey[50],
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text("7998*****",
+                                              color: Colors.grey[50],
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text("2g",
+                                            style: TextStyle(
+                                                color: Colors.grey[50])),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "IMEI: ",
                                           style: TextStyle(
-                                              color: Colors.grey[50])),
-                                    ],
-                                  ),
-                                ],
+                                              color: Colors.grey[50],
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text("7998*****",
+                                            style: TextStyle(
+                                                color: Colors.grey[50])),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                          Positioned(
-                              bottom: mq.height * 0.06,
-                              left: mq.width * 0.1,
-                              child: IconButton(
-                                  onPressed: () {},
-                                  icon: Container(
-                                    padding: const EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: Colors.white, width: 1),
-                                      borderRadius: BorderRadius.circular(16),
-                                      color: Colors.transparent,
-                                    ),
-                                    child: const Icon(
-                                      Icons.navigation_outlined,
-                                      color: Colors.white,
-                                    ),
-                                  ))),
-                          Positioned(
-                              right: 0,
-                              bottom: 0,
-                              child: CachedNetworkImage(
-                                imageUrl: urlProvider.urlMap[
-                                    'assets/images/content/tracker_2g.png']!,
-                                placeholder: null,
-                                errorWidget: null,
-                              )),
-                          Positioned(
-                            top: mq.height * 0.03,
-                            right: mq.width * 0.05,
-                            child: BatteryIndicator(
-                              value: 0.6,
-                              barColor: Colors.white,
-                              trackColor: Colors.grey[500],
+                            Positioned(
+                                bottom: mq.height * 0.06,
+                                left: mq.width * 0.1,
+                                child: IconButton(
+                                    onPressed: () {},
+                                    icon: Container(
+                                      padding: const EdgeInsets.all(8),
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.white, width: 1),
+                                        borderRadius: BorderRadius.circular(16),
+                                        color: Colors.transparent,
+                                      ),
+                                      child: const Icon(
+                                        Icons.navigation_outlined,
+                                        color: Colors.white,
+                                      ),
+                                    ))),
+                            Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: CachedNetworkImage(
+                                  imageUrl: urlProvider.urlMap[
+                                      'assets/images/content/tracker_2g.png']!,
+                                  placeholder: null,
+                                  errorWidget: null,
+                                )),
+                            Positioned(
+                              top: mq.height * 0.03,
+                              right: mq.width * 0.05,
+                              child: cbi.BatteryIndicator(
+                                value: 0.6,
+                                barColor: Colors.white,
+                                trackColor: Colors.grey[500],
+                              ),
                             ),
-                          ),
-                          Positioned(
-                              bottom: mq.height * 0.02,
-                              left: mq.width * 0.06,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 5),
-                                decoration: BoxDecoration(
-                                  border:
-                                      Border.all(color: Colors.white, width: 1),
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.transparent,
-                                ),
-                                child: const Text(
-                                  "Rohini Sec-4",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ))
-                        ],
+                            Positioned(
+                                bottom: mq.height * 0.02,
+                                left: mq.width * 0.06,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 5),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.white, width: 1),
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: Colors.transparent,
+                                  ),
+                                  child: const Text(
+                                    "Rohini Sec-4",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ))
+                          ],
+                        ),
                       ),
                     );
                   }),
